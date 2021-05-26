da9fa7f7e881094784688800e410f189e5c1977c
Dids-Himanshu <himanshudidwaniya98@gmail.com>
2021-05-26 23:55:58 +0530

    first commit

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$phoneno = $_POST['phoneno'];
+$message = $_POST['message'];
+
+$subject = "Contact Form";
+$email_body = "User Name: $name.\n".
+                "User Email: $email.\n".
+                "User Message:$message.\n".
+                "User Phoneno:$phoneno.\n";
+$to = "himanshudidwaniyajsk@gmail.com";
+$headers = "From: $email \r\n";
+$headers .="Reply-To:$email \r\n"; 
+mail($to, $subject, $email_body, $headers) or die("Error!");
+echo "Thank You!";
+header("Location:index.html");
+?>
